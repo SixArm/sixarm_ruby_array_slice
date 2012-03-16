141adae5693dd2d59cacc727167b590f19890a30
SixArm <sixarm@sixarm.com>
2012-03-16 13:50:04 -0700

    Upgrade for Ruby 1.9.3, minitest/spec, and improved docs

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Install:
 
 Bundler:
 
-    gem "sixarm_ruby_array_slice", "=2.1.2"
+    gem "sixarm_ruby_array_slice", "=2.1.4"
 
 Require:
 
@@ -42,3 +42,37 @@ Example of both ways to slice:
     [1,2,3,4,5,6,7,8].slice_by_share(2) 
     => [[1,2,3,4],[5,6,7,8]] 
 
+
+## Changes
+
+* 2012-03-16 2.1.4 1.9.3; minitest/spec; update docs, tests.
+* 2012-02-01 1.0.0 Publish.
+
+
+## License
+
+You may choose any of these open source licenses:
+
+  * Apache License
+  * BSD License
+  * CreativeCommons License, Non-commercial Share Alike
+  * GNU General Public License Version 2 (GPL 2)
+  * GNU Lesser General Public License (LGPL)
+  * MIT License
+  * Perl Artistic License
+  * Ruby License
+
+The software is provided "as is", without warranty of any kind, 
+express or implied, including but not limited to the warranties of 
+merchantability, fitness for a particular purpose and noninfringement. 
+
+In no event shall the authors or copyright holders be liable for any 
+claim, damages or other liability, whether in an action of contract, 
+tort or otherwise, arising from, out of or in connection with the 
+software or the use or other dealings in the software.
+
+This license is for the included software that is created by SixArm;
+some of the included software may have its own licenses, copyrights, 
+authors, etc. and these do take precedence over the SixArm license.
+
+Copyright (c) 2005-2013 Joel Parker Henderson
--- test/sixarm_ruby_array_slice_test.rb
+++ test/sixarm_ruby_array_slice_test.rb
@@ -4,102 +4,104 @@ require 'simplecov'
 SimpleCov.start
 require 'sixarm_ruby_array_slice'
 
-class SliceTest < Test::Unit::TestCase
+describe Array do
 
- A=[1,2,3,4,5,6,7,8]
+  before do
+    A ||= [1,2,3,4,5,6,7,8]
+  end
 
- ### #slice_by_size typical use
+  describe "#slice_by_size" do
 
- def test_slice_by_size_with_1_count
-   assert_equal([[1],[2],[3],[4],[5],[6],[7],[8]], A.slice_by_size(1))
- end
+    it "with 1 count" do
+      A.slice_by_size(1).must_equal [[1],[2],[3],[4],[5],[6],[7],[8]]
+    end
 
- def test_slice_by_size_with_balanced_results
-   assert_equal([[1,2],[3,4],[5,6],[7,8]], A.slice_by_size(2))
-   assert_equal([[1,2,3,4],[5,6,7,8]], A.slice_by_size(4))
- end
+    it "with balanced results" do
+      A.slice_by_size(2).must_equal [[1,2],[3,4],[5,6],[7,8]]
+      A.slice_by_size(4).must_equal [[1,2,3,4],[5,6,7,8]]
+    end
 
- def test_slice_by_size_with_unbalanced_results
-   assert_equal([[1,2,3],[4,5,6],[7,8]], A.slice_by_size(3))
- end
+    it "with unbalanced results" do
+      A.slice_by_size(3).must_equal [[1,2,3],[4,5,6],[7,8]]
+    end
 
- def test_slice_by_size_with_very_unbalanced_results
-   assert_equal([[1,2,3,4,5,6,7],[8]], A.slice_by_size(7))
- end
+    it "with very unbalanced results" do
+      A.slice_by_size(7).must_equal [[1,2,3,4,5,6,7],[8]]
+    end
 
- ### ... atypical
+    it "with empty input" do
+      [].slice_by_size(1).must_equal []
+    end
 
- def test_slice_by_size_with_empty_input
-   assert_equal([],[].slice_by_size(1))
- end
+    it "with 0 count" do
+      assert_raise ArgumentError do
+        A.slice_by_size(0)
+      end
+    end
 
- def test_slice_by_size_with_0_count
-   assert_raise ArgumentError do
-     A.slice_by_size(0)
-   end
- end
+    it "with negative count" do
+      assert_raise ArgumentError do
+        A.slice_by_size(-1)
+      end
+    end
 
- def test_slice_by_size_with_negative_count
-   assert_raise ArgumentError do
-     A.slice_by_size(-1)
-   end
- end
+    it "with float count" do
+      assert_raise ArgumentError do
+        A.slice_by_size(1.234)
+      end
+    end
 
- def test_slice_by_size_with_float_count
-   assert_raise ArgumentError do
-     A.slice_by_size(1.234)
-   end
- end
+    it "with non numeric count" do
+      assert_raise ArgumentError do
+        A.slice_by_size(1.234)
+      end
+    end
 
- def test_slice_by_size_with_non_numeric_count
-   assert_raise ArgumentError do
-     A.slice_by_size(1.234)
-   end
- end
+  end
 
- ### #slice_by_share typical use
+  describe "#slice_by_share" do
 
- def test_slice_by_share_with_1_count
-   assert_equal([[1,2,3,4,5,6,7,8]],A.slice_by_share(1))
- end
+    it "with 1 count" do
+      A.slice_by_share(1).must_equal [[1,2,3,4,5,6,7,8]]
+    end
 
- def test_slice_by_share_with_balanced_results
-   assert_equal([[1,2,3,4],[5,6,7,8]], A.slice_by_share(2))
-   assert_equal([[1,2],[3,4],[5,6],[7,8]], A.slice_by_share(4))
- end
+    it "with balanced results" do
+      A.slice_by_share(2).must_equal [[1,2,3,4],[5,6,7,8]]
+      A.slice_by_share(4).must_equal [[1,2],[3,4],[5,6],[7,8]]
+    end
 
- def test_slice_by_share_with_unbalanced_results
-   assert_equal([[1,2,3],[4,5,6],[7,8]], A.slice_by_share(3))
- end
+    it "with unbalanced results" do
+      A.slice_by_share(3).must_equal [[1,2,3],[4,5,6],[7,8]]
+    end
 
- def test_slice_by_share_with_very_unbalanced_results
-   assert_equal([[1,2],[3,4],[5,6],[7,8],[],[],[]], A.slice_by_share(7))
- end
+    it "with very unbalanced results" do
+      A.slice_by_share(7).must_equal [[1,2],[3,4],[5,6],[7,8],[],[],[]]
+    end
 
- ### ...atypical
+    it "with zero count" do
+      assert_raise ArgumentError do
+        A.slice_by_share(0)
+      end
+    end
 
- def test_slice_by_share_with_zero_count
-   assert_raise ArgumentError do
-     A.slice_by_share(0)
-   end
- end
+    it "with negative count" do
+      assert_raise ArgumentError do
+        A.slice_by_share(-1)
+      end
+    end
 
- def test_slice_by_share_with_negative_count
-   assert_raise ArgumentError do
-     A.slice_by_share(-1)
-   end
- end
+    it "with float count" do
+      assert_raise ArgumentError do
+        A.slice_by_share(1.234)
+      end
+    end
 
- def test_slice_by_share_with_float_count
-   assert_raise ArgumentError do
-     A.slice_by_share(1.234)
-   end
- end
+    it "with non numeric count" do
+      assert_raise ArgumentError do
+        A.slice_by_share("")
+      end
+    end
 
- def test_slice_by_share_with_non_numeric_count
-   assert_raise ArgumentError do
-     A.slice_by_share("")
-   end
- end
+  end
 
 end
